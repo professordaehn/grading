ef6886c22fd3498be230c46e3d81eebedb3b82d7
Mr. Daehn <jdaehn@missouristate.edu>
2018-02-03 20:06:54 -0600

    GRADING-CSC131SP18 - Initial set of unit tests.

--- csc131/grade_tools.py
+++ csc131/grade_tools.py
@@ -3,32 +3,54 @@
 # Written by Jim Daehn <jdaehn at missouristate.edu>
 # Copyright (C) 2018 JRDevelopment
 
-from sys import argv
+import subprocess
+import sys
+
+import os
 
 EXPECTED_NUM_ARGS = 4
 ASSIGNMENT_ARG_INDEX = 1
 ASSIGNMENT_NUMBER_ARG_INDEX = 2
 USERNAME_ARG_INDEX = 3
 COMMAND_LINE_SYNTAX_ERROR_CODE = 2
+EXIT_SUCCESS = 0
 
 
 def print_usage() -> None:
+    """
+    Print the general usage of this script to the terminal window.
+    :return: None
+    """
     print("Usage:")
     return None
 
 
-def clone_repo(repo: str) -> None:
+def clone_repo(repo=None) -> bool:
+    """
+    Clones the given repo into the current directory.
+    :param repo: The name of the repository to clone.
+    :return: True is returned upon successful clone; False otherwise.
+    """
+    if repo is None:
+        return False
     print("Cloning repo:", repo)
-    return None
+    git_clone_cmd = ["git", "clone", repo]
+    current_process = subprocess.run(git_clone_cmd, stdout=subprocess.PIPE, encoding="utf-8")
+    try:
+        slash_location = repo.index('/') + 1
+    except ValueError:
+        return False
+    return os.path.isdir(repo[slash_location:-4])
 
 
 def main() -> int:
-
-    clone_repo()
+    """
+    Execution entry point for this script.
+    :return: EXIT_SUCCESS is returned upon successful execution of this script.
+    """
+    clone_repo(None)
 
 
 if __name__ == '__main__':
-    if len(argv) == EXPECTED_NUM_ARGS:
-        main()
-    else:
-        print_usage()
+    assert not hasattr(sys.stdout, "getvalue")
+    main()
---
+++ csc131/grade_tools_test.py
@@ -0,0 +1,54 @@
+import unittest
+import os
+import subprocess
+import shutil
+
+from csc131 import grade_tools
+from sys import stdout
+
+
+class MyTestCase(unittest.TestCase):
+    """
+    Unit test cases for grade_tools module.
+    """
+    REAL_REPO = "git@github.com:msu-csc131/lab01-msu-csc-sdent.git"
+    BOGUS_REPO = "I'm not really a repo"
+    CLONED_REPO = "lab01-msu-csc-sdent"
+
+    def tearDown(self):
+        """
+        TODO: Figure out how to delete the cloned directory
+        :return:
+        """
+        if os.path.isdir(self.CLONED_REPO):
+            rmdir_cmd = ["rm", "-rf", self.CLONED_REPO]
+            p = subprocess.run(rmdir_cmd, stdout=subprocess.PIPE, encoding="utf-8")
+            print("p.check_returncode() =", p.check_returncode())
+
+    def test_clone_repo_returns_false_for_none(self):
+        """
+        Validate that clone_repo() returns False when repo is None
+        :return: None
+        """
+        self.assertFalse(grade_tools.clone_repo(None))
+
+    def test_clone_repo_returns_false_for_bogus_repo(self):
+        """
+        Validate the clone_repo() returns False when a repo doesn't exist.
+        :return: None
+        """
+        self.assertFalse(grade_tools.clone_repo(self.BOGUS_REPO))
+
+    def test_clone_repo_returns_true_upon_success(self):
+        """
+        Validate the clone_repo() returns True when it successfully clones a repo.
+        git@github.com:msu-csc131/lab01-msu-csc-sdent.git
+        git@github.com:msu-csc131/hw-01-file-concordance-msu-csc-sdent.git
+        :return: None
+        """
+        self.assertTrue(grade_tools.clone_repo(self.REAL_REPO))
+
+
+if __name__ == '__main__':
+    assert not hasattr(stdout, "getvalue")
+    unittest.main(module=__name__, buffer=True, exit=False)
