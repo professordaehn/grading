de01102adf12d79484f48ced8e678434cccb0d4a
Jim Daehn <jdaehn@missouristate.edu>
2020-02-27 21:56:52 -0600

    Add feature method for https formatting.

--- specs/msu/csc232/grade_one_assignment_spec.py
+++ specs/msu/csc232/grade_one_assignment_spec.py
@@ -1,17 +1,20 @@
 from argparse import Namespace
+
+import mock as mock
+
 from msu.csc232.grade_one_assignment import get_repo_url
 from nimoy.specification import Specification
 
 
 class GradeOneAssignmentSpec(Specification):
     """
-    A Nimoy spec.
+    A Nimoy spec to test the GradeOneAssignment module.
     """
 
     def get_repo_url_formats_ssh_method(self) -> None:
         """
         Feature method to test that get_repo_url method formats the
-        proper url for cloning.
+        proper url for cloning when cloning via ssh.
         :return: None
         """
         with given:
@@ -20,3 +23,16 @@ class GradeOneAssignmentSpec(Specification):
             url = get_repo_url(namespace)
         with then:
             url == 'git@github.com:msu-csc232-sp20/lab04-title-username.git'
+
+    def get_repo_url_formats_https_method(self) -> None:
+        """
+        Feature method to test that get_repo_url method formats the
+        proper url for cloning when cloning via https.
+        :return: None
+        """
+        with given:
+            namespace = Namespace(category='lab', number='04', username='username', title='title', method='https')
+        with when:
+            url = get_repo_url(namespace)
+        with then:
+            url == 'https://github.com/msu-csc232-sp20/lab04-title-username.git'
